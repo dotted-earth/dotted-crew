65fad4c0493db0e8fae3e9405efa35316f58678d
Billy Le <hi@billyle.dev>
2024-05-08 18:53:46 +0800

    add docker compose watch. fix itinerary data on POST request

--- src/models/itinerary.py
+++ src/models/itinerary.py
@@ -1,13 +1,14 @@
+import datetime
 from pydantic import BaseModel
 from typing import Union
 
 
 class Itinerary(BaseModel):
-    id: str
-    created_date: str
-    start_date: str
-    end_date: str
-    length_of_stay: str
+    id: int
+    created_at: datetime.datetime
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+    length_of_stay: int
     destination: str
     budget: Union[int, None]
     itinerary_status: str
