9cea78b7c0f585e9b797f520168361b82ccc1de4
Billy Le <hi@billyle.dev>
2024-05-09 17:51:45 +0800

    force json output

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 from src.crew import DottedCrew
 from src.models.generate_itinerary_request_body import GenerateItineraryRequestBody
@@ -11,4 +12,4 @@ async def generate_itinerary(data: GenerateItineraryRequestBody):
 
     dotted_crew = DottedCrew()
     result = await dotted_crew.run(data)
-    return result
+    return json.loads(result)
--- src/tasks/itinerary_tasks.py
+++ src/tasks/itinerary_tasks.py
@@ -19,7 +19,7 @@ class ItineraryTasks:
                 f"""Generate a {itinerary.length_of_stay}-day itinerary for a trip to {itinerary.destination} starting {itinerary.start_date} to {itinerary.end_date}. The itinerary must include breakfast, lunch, and dinner and at least 2 activities. Also include mode of transportation, distance from point ot point, and duration of each activity, and commuting. Make sure to use the most recent data as possible. When working with your co-workers, give them the context all the itinerary {itinerary.model_dump_json()}"""
             ),
             agent=agent,
-            expected_output="A JSON Output of the itinerary.",
+            expected_output="Output in only JSON format. Each day should be in an array.",
             # output_json=ItineraryDraft,
             context=context,
             async_execution=False,
